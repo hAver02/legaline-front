903715e39345cae0a4c2d3b25055e2b392d5ac3e
luchi <pazluciano.dev@gmail.com>
2024-02-28 17:59:31 -0300

    https

--- src/hooks/useAuthValidation.jsx
+++ src/hooks/useAuthValidation.jsx
@@ -9,7 +9,7 @@ export function useAuthValidation (){
     const { setCasos } = useContext(CasosContext)
     useEffect(() => {
         if(idUser == "") return 
-        fetch(`http://85.31.61.130:3000/user/${idUser}`)
+        fetch(`https://85.31.61.130:3000/user/${idUser}`)
         .then(res => res.json())
         .then(data => {
             if(!data.ok) return // Mostrar algun tipo de error.
