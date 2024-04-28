dc924eee5a847c4e5b862ffe89c257b184735657
luchi <pazluciano.dev@gmail.com>
2024-04-28 10:04:08 -0300

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseUrl = import.meta.env.VITE_BASE_URL || "https://srv471383.hstgr.cloud:3000/";
 const instance = axios.create({
     
-    baseURL : "http://localhost:3000/",
+    baseURL : baseUrl,
     withCredentials : true,
 
 })
